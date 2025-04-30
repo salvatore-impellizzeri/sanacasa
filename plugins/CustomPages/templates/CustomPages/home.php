6a48e7a5966ae644da96ffa512ad6ff5c79255d3
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-04-30 15:01:41 +0200

    first commit

---
+++ plugins/CustomPages/templates/CustomPages/home.php
@@ -0,0 +1,16 @@
+<div class="home">
+    <div class="home__hero">
+        <video src="img/video/home-video.mp4" autoplay loop muted playsinline></video>
+        <div class="home__hero__content">
+            <span class="font-chillax font-24 fw-medium">
+                la sanatoria di casa, semplice
+            </span>
+            <h1 class="title-primary">
+                Il modo più semplice per gestire la tua sanatoria edilizia e catastale.
+            </h1>
+            <p class="font-20">
+                Architetto dedicato, sopralluogo e rilievo, verifica edilizia e catastale, sanatoria dell’immobile, regolarizzazione catastale, certificazione di conformità. 
+            </p>
+        </div>
+    </div>
+</div>
---
+++ templates/element/header.php
@@ -0,0 +1,50 @@
+<?php
+use Cake\Core\Configure;
+
+$extraClass = $extraClass ?? '';
+$homeLink = ACTIVE_LANGUAGE == DEFAULT_LANGUAGE ? '/' : '/'.ACTIVE_LANGUAGE.'/';
+$languages = Configure::read('Setup.languages');
+?>
+
+<header class="header">
+    <div class="header__inner">
+        <a href="<?= $homeLink ?>" class="header__logo">
+            <?= $this->Frontend->svg('logo.svg') ?>
+        </a>
+    
+        <?php if (!empty($showSearch)): ?>
+            <div class="header__search">
+                <?php echo $this->element('search'); ?>
+            </div>
+        <?php endif; ?>
+        
+        <nav class="header__menu">
+            <?= $this->cell('Menu.Menu', [1]) ?>
+        </nav>
+        <div class="header__hamburger">
+            <?php echo $this->element('hamburger'); ?>
+        </div>
+    
+    
+        <?php if (!empty($languages)): ?>
+            <div class="header__languages">
+                <?= $this->element('languages'); ?>
+            </div>
+        <?php endif; ?>
+    
+        <?php if (!empty($cta)): ?>
+            <div class="header__cta">
+                <?= $this->element('cta', [
+                    'label' => 'Inizia ora',
+                    'url' => '#',
+                ]); ?>
+            </div>
+        <?php endif; ?>
+        
+        <?php if (empty($hamburgerMenu)): ?>
+            <div class="header__hamburger-mobile">
+                <?php echo $this->element('hamburger'); ?>
+            </div>
+        <?php endif; ?>
+    </div>
+</header>
