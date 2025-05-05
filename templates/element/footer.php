d72f408a93f41e3add4e8eea7d097dcb5af8ccf7
Salvatore <>
2025-05-05 16:21:57 +0200

    footer

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -1,26 +1,38 @@
 <footer class="footer">
-
-    <div class="footer__info">
-        <?= $this->element('snippet', ['id' => 1])?>
+    <div class="footer__container m-auto">
+        <div class="footer__text">
+            <p class="fw-light font-24">
+                SANACASA non è uno studio di architettura. È un’agenzia che ti permette di gestire le regolarizzazioni del tuo immobile online, con un portale e un servizio di assistenza tecnica dedicata. Tutte le attività riservate sono svolte dagli Architetti/Ingegneri/Geometri partner di SANACASA, iscritti ai relativi Albi. 
+            </p>
+            <?= $this->element('cta', [
+                'label' => 'Review us on Trustpiolt',
+                'url' => '#',
+                'extraClass' => 'cta--trustpilot'
+            ]); ?>
+            <div class="footer__legal">
+                <ul class="menu">
+                    <li><?= $this->Frontend->seolink(__d('policies', 'Privacy policy'), '/policies/view/1'); ?></li>
+                    <li><?= $this->Frontend->seolink(__d('policies', 'Cookie policy'), '/policies/view/2'); ?></li>
+                    <li><span id="cookie_reload"><?php echo __d('policies', 'manage cookies'); ?></span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer__links">
+            <div class="footer__menu">
+                <?= $this->cell('Menu.Menu', [8]) ?>
+            </div>
+            <?= $this->element('cta', [
+                'label' => 'Inizia ora',
+                'url' => '#',
+                'extraClass' => 'cta--primary'
+            ]); ?>
+        </div>
     </div>
-    
-    <div class="footer__menu">
-        <?= $this->cell('Menu.Menu', [8]) ?>
-    </div>
-
-    <div class="footer__legal">
-        <ul class="menu">
-            <li><?= $this->Frontend->seolink(__d('policies', 'Privacy policy'), '/policies/view/1'); ?></li>
-            <li><?= $this->Frontend->seolink(__d('policies', 'Cookie policy'), '/policies/view/2'); ?></li>
-            <li><span id="cookie_reload"><?php echo __d('policies', 'manage cookies'); ?></span></li>
-        </ul>
-        
-    </div>
-
-    <div class="footer__social">
-        <?= $this->cell('Menu.Menu', [7]) ?>
-    </div>
-    <div class="footer__credits">
-        <a href="https://www.webmotion.it" title="Web Agency Verona">Credits</a>
+    <div class="footer__img">
+        <?= $this->Frontend->svg('logo.svg'); ?>
     </div>
 </footer>
+
+    <!-- <div class="footer__info">
+        <?= $this->element('snippet', ['id' => 1])?>
+    </div>
