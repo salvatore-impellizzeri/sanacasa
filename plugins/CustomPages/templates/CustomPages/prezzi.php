085eeab4f979599b1f880acfec6aee859898b295
Salvatore <>
2025-05-06 15:55:46 +0200

    cards price (da sistemare)

--- plugins/CustomPages/templates/CustomPages/prezzi.php
+++ plugins/CustomPages/templates/CustomPages/prezzi.php
@@ -1,9 +1,51 @@
 <div>
-    <div class="pt-h bg-secondary">
-        <?= $this->element('title', [
-            'label' => 'prezzi',
-            'title' => 'Scegli il servizio in <br> base alla tua esigenza',
-            'extraClass' => "title--white text-center mb-105",
-        ]); ?>
+    <div class="pt-h bg-secondary pb-168">
+        <div class="container-xl m-auto">
+            <?= $this->element('title', [
+                'label' => 'prezzi',
+                'title' => 'Scegli il servizio in <br> base alla tua esigenza',
+                'extraClass' => "title--white text-center mb-105",
+            ]); ?>
+            <?php 
+                $prices = [
+                    [
+                        'type' => "Regolarizzazione <br> catastale",
+                        'price' => "299",
+                        'url' => '#'
+                    ],
+                    [
+                        'type' => "Sanatoria <br> edilizia-urbanistica",
+                        'price' => "899",
+                        'url' => '#'
+                    ]
+                ]
+            ?>
+            <div class="boxes">
+                <?php foreach ($prices as $price) { ?>
+                    <div class="box box--price">
+                        <div class="box--price__upper">
+                            <h2 class="font-48 fw-bold">
+                                <?= $price['type'] ?>
+                            </h2>
+                        </div>
+                        <div class="box--price__lower">
+                            <div>
+                                <div class="font-18">
+                                    A partire da
+                                </div>
+                                <div>
+                                    <span class="fw-bold font-64">€ <?= $price['price'] ?></span><span class="font-18">/ unità</span>
+                                </div>
+                            </div>
+                            <?= $this->element('cta', [
+                                'label' => 'Dettaglio prezzi',
+                                'url' => $price['url'],
+                                'extraClass' => 'cta--secondary'
+                            ]); ?>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
     </div>
 </div>
--- templates/element/box-slider.php
+++ templates/element/box-slider.php
@@ -1,6 +1,6 @@
-<div class="box-slider bg-purple">
-    <div class="box-slider__text">
-        <div class="box-slider__num font-64 fw-semibold font-chillax">
+<div class="box box bg-purple">
+    <div class="box__text">
+        <div class="box__num font-64 fw-semibold font-chillax">
             <?= $num ?>
         </div>
         <h3 class="fw-bold font-48">
@@ -10,7 +10,7 @@
             <?= $text ?>
         </p>
     </div>
-    <div class="box-slider__img">
+    <div class="box__img">
         <img src="<?= $img ?>" alt="<?= $title ?>">
     </div>
 </div>
