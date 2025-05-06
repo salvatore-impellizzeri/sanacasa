d0d87c4fd08a17df317fe198612280dc9f593457
Salvatore <>
2025-05-06 15:00:24 +0200

    sistemato il file spaces

---
+++ plugins/CustomPages/templates/CustomPages/prezzi.php
@@ -0,0 +1,9 @@
+<div>
+    <div class="pt-h bg-secondary">
+        <?= $this->element('title', [
+            'label' => 'prezzi',
+            'title' => 'Scegli il servizio in <br> base alla tua esigenza',
+            'extraClass' => "title--white text-center mb-105",
+        ]); ?>
+    </div>
+</div>
