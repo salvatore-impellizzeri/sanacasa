ba3e5a28e7e45e67398f9d433821821c43324c3c
Salvatore <>
2025-05-07 16:43:07 +0200

    completata la sezione news (non funziona ancora il bottone)

--- plugins/CustomPages/templates/CustomPages/news.php
+++ plugins/CustomPages/templates/CustomPages/news.php
@@ -1,73 +1,84 @@
 <div class="pt-h">
-    <?= $this->element('title', [
-        'label' => 'news',
-        'title' => 'Le ultime notizie e insights <br> dal mondo della sanatoria',
-        'extraClass' => "title--primary text-center mb-105",
-    ]); ?>
-    <?php
-        $articles = [
-            [
-                'img' => 'img/img7.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-            [
-                'img' => 'img/img8.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-            [
-                'img' => 'img/img9.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-            [
-                'img' => 'img/img7.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-            [
-                'img' => 'img/img8.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-            [
-                'img' => 'img/img9.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-            [
-                'img' => 'img/img7.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-            [
-                'img' => 'img/img8.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-            [
-                'img' => 'img/img9.png',
-                'date' => '10.04.25',
-                'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
-            ],
-        ]
-    ?>
-    <div class="grid-cols-3 container-xl m-auto gap-37 gap-y-62">
-        <?php foreach ($articles as $article) { ?>
-            <div class="card card--articles">
-                <div class="card--articles__img">
-                    <img src="<?= $article['img'] ?>" alt="Immagine Articolo">
-                </div>
-                <div class="card--articles__text">
-                    <div class="font-20 card--articles__date fw-medium">
-                        <?= $article['date'] ?>
+    <div class="pb-134">
+        <?= $this->element('title', [
+            'label' => 'news',
+            'title' => 'Le ultime notizie e insights <br> dal mondo della sanatoria',
+            'extraClass' => "title--primary text-center mb-105",
+        ]); ?>
+        <?php
+            $articles = [
+                [
+                    'img' => 'img/img7.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+                [
+                    'img' => 'img/img8.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+                [
+                    'img' => 'img/img9.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+                [
+                    'img' => 'img/img7.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+                [
+                    'img' => 'img/img8.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+                [
+                    'img' => 'img/img9.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+                [
+                    'img' => 'img/img7.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+                [
+                    'img' => 'img/img8.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+                [
+                    'img' => 'img/img9.png',
+                    'date' => '10.04.25',
+                    'text' => 'Difformità opere interne: CILA in sanatoria, come sanare e quanto costa?'
+                ],
+            ]
+        ?>
+        <div class="grid-cols-3 container-xl m-auto gap-37 gap-y-62 mb-96">
+            <?php foreach ($articles as $article) { ?>
+                <div class="card card--articles">
+                    <div class="card--articles__img">
+                        <img src="<?= $article['img'] ?>" alt="Immagine Articolo">
                     </div>
-                    <h3>
-                        <?= $article['text'] ?>
-                    </h3>
+                    <div class="card--articles__text">
+                        <div class="font-20 card--articles__date fw-medium">
+                            <?= $article['date'] ?>
+                        </div>
+                        <h3>
+                            <?= $article['text'] ?>
+                        </h3>
+                    </div>
                 </div>
-            </div>
-        <?php } ?>
+            <?php } ?>
+        </div>
+        <div class="text-center">
+            <?= $this->element('cta', [
+                'label' => 'Carica altri articoli',
+                'extraClass' => 'cta--primary'
+            ]); ?>
+        </div>
     </div>
+
+     <!-- FORM -->
+     <?= $this->element('form'); ?>
 </div>
