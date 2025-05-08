622925d7d467e09787578e15b597efa5ca250c94
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-05-08 10:32:15 +0200

    FAQ Completata (da fare la form)

---
+++ plugins/CustomPages/templates/CustomPages/faq.php
@@ -0,0 +1,50 @@
+<div class="pt-h faq-page">
+    <div class="container-title m-auto">
+        <?= $this->element("title", [
+            "label" => "FAQ",
+            "title" => "Domande frequenti",
+            "extraClass" => "title--primary text-center mb-105",
+        ]); ?>
+    </div>
+    <div class="pb-100">
+        <div class="container-faq m-auto mb-96">
+            <div class="faq-page__accordion">
+                <?= $this->element("faq", [
+                    'faqs' => [
+                        [
+                            "title" => "Cosa comprende esattamente il servizio di SANACASA?", 
+                            "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, provident maiores cupiditate earum magni, temporibus ad eaque, distinctio suscipit accusamus molestiae rem corporis. Odit minus aliquid suscipit magni eos similique!"
+                        ],
+                        [
+                            "title" => "Devo spostarmi o prendere appuntamenti fisici per la regolarizzazione?", 
+                            "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, provident maiores cupiditate earum magni, temporibus ad eaque, distinctio suscipit accusamus molestiae rem corporis. Odit minus aliquid suscipit magni eos similique!"
+                        ],
+                        [
+                            "title" => "Chi si occuperà della mia pratica di regolarizzazione?", 
+                            "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, provident maiores cupiditate earum magni, temporibus ad eaque, distinctio suscipit accusamus molestiae rem corporis. Odit minus aliquid suscipit magni eos similique!"
+                        ],
+                        [
+                            "title" => "Quali sono i tempi necessari per completare una regolarizzazione con SANACASA?", 
+                            "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, provident maiores cupiditate earum magni, temporibus ad eaque, distinctio suscipit accusamus molestiae rem corporis. Odit minus aliquid suscipit magni eos similique!"
+                        ],
+                        [
+                            "title" => "Qunato costa il servizio di SANACASA?", 
+                            "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, provident maiores cupiditate earum magni, temporibus ad eaque, distinctio suscipit accusamus molestiae rem corporis. Odit minus aliquid suscipit magni eos similique!"
+                        ],
+                    ] 
+                ]) ?>
+            </div>
+            <div class="faq-page__img">
+                <img src="img/img7.png" alt="Immagine FAQ">
+            </div>
+        </div>
+        <div class="text-center">
+            <?= $this->element('cta', [
+                'label' => "Vai alla form",
+                'url' => "#",
+                'extraClass' => "cta--primary"
+            ]); ?>
+        </div>
+    </div>
+    <?= $this->element('form') ?>
+</div>
--- templates/element/faq.php
+++ templates/element/faq.php
@@ -1,9 +1,11 @@
-<div class="faq" data-accordion>
-    <div class="faq__toggler" data-accordion-toggler>
-        <h3 class="faq__title"><?= $faq->title ?></h3>
-        <span class="faq__toggler__indicator"></span>
+<?php foreach ($faqs as $faq) { ?>
+    <div class="faq" data-accordion>
+        <div class="faq__toggler" data-accordion-toggler>
+            <h3 class="faq__title"><?= $faq['title'] ?></h3>
+            <span class="faq__toggler__indicator"></span>
+        </div>
+        <div class="faq__content" data-accordion-content>
+            <div class="faq__paragraph font-18"><?= $faq['excerpt'] ?></div>
+        </div>
     </div>
-    <div class="faq__content" data-accordion-content>
-        <div class="faq__paragraph"><?= $faq->excerpt ?></div>
-    </div>
-</div>
+<?php } ?>
